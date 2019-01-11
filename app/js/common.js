324d62725c171c95d16256df244d7c837b9e43eb
Александр Саблин <sablin.irk@gmail.com>
2019-01-11 11:29:33 +0800

    new project

--- app/js/common.js
+++ app/js/common.js
@@ -1,99 +1,27 @@
-$(function() {
-	$(".food-carousel").owlCarousel({
-		items: 4,
-		nav: true,
-		loop: true,
-		navText : ["",""]
-	});
-
-	$(".owl-carousel").owlCarousel({
-		items: 1,
-		nav: true,
-		loop: true,
-		navText : ["",""]
-	});
-
-
-	// специально делаю без jquery
-
-	let stockTabs = document.querySelectorAll('.promo__tab'),
-		stockLink = document.querySelectorAll('.stock-link');
-
-		for(let i=0; i<stockLink.length;i++){
-			stockLink[i].addEventListener('click', funTabs);
+window.onload= function(){
+	// создадим обьект vue с массивом из чекбоксов
+	let form = new Vue({
+		el: '.form',
+		data: {
+			boxes:['БЭМ/OOCSS', 'Gulp/Grunt', 'Использую Git',
+					'Pug(Jade)', 'Webpack', 'С глазомером все ок', 'Stylus/LESS/SASS',
+					'Дружу с WebGL' , 'Читаю blog.csssr.ru', 'Работаю с SVG' , 'JQuery',
+				'Я ленивый(-ая)', 'Верстаю семантично', 'Знаю/изучаю Angular',
+			'У меня хороший английский' , 'Accessibility(A11Y)','Знаю/изучаю React', 'ES2015/ES2016', 'Знаю/изучаю Node.js']
 		}
-		
+	})
 
-		function funTabs(e){
+	// слайдер
+
+	let links = document.querySelectorAll('.slider__wrapper-links a')
+		sliderDot = document.querySelector('.slider-dot');
+
+	for(let i =0; i< links.length; i++){
+		links[i].addEventListener('click', function(e){
 			e.preventDefault();
-			for(let i =0; i<stockLink.length;i++){
-				stockLink[i].classList.remove('active-link');
-			}
-			this.classList.add('active-link');
-			let dataTab = e.target.getAttribute('data-tab');
-			console.log(dataTab);
-			for(let i=0; i < stockTabs.length; i++){
-				if(dataTab == i){
-					stockTabs[i].classList.remove('hide');
-				}else{
-					stockTabs[i].classList.add('hide');
-				}
-			}
-
-		
-		}
-
-		let plus = document.querySelectorAll('.plus')
-			mines = document.querySelectorAll('.mines');
-
-
-		for(let i =0; i < plus.length; i++){
-			plus[i].addEventListener('click', changeNum(1))
-		};
-
-		for(let i =0; i < mines.length; i++){
-			mines[i].addEventListener('click',  changeNum(-1))
-		}
-
-		function changeNum(d){
-			return function(e){
-				e.preventDefault();
-				let num = parseInt(this.parentElement.querySelector('.num').innerHTML);
-				console.log(num)
-				num += d;
-				if(num<=0){
-					num = 1;
-				}
-				this.parentElement.querySelector('.num').innerHTML = num;
-				
-			}
-		
-		}
-
-		let callback = document.querySelector('.callback');
-
-		callback.addEventListener('click', function(){
-			alert('Должно появиться красивое всплывающее окно. Но доделать его не успел. По этому вот пока снеговик: СНЕГОВИК');
+			sliderDot.style.left = links[i].getAttribute('data-left');
 		})
-		
-		
-		
-		let nav = document.querySelector('header nav'),
-		block = document.querySelector('.nav-clone');
-		let box = nav.getBoundingClientRect();
-		let top = box.top + pageYOffset;
-			
-		document.addEventListener('scroll', function(){
-			if(window.pageYOffset > top){
-				nav.classList.add('fixed');
-				block.style.display = 'block';
-			}else{
-				nav.classList.remove('fixed');
-				block.style.display = 'none';
-			}
+	}
 
-		})
+}
 
-
-
-});
